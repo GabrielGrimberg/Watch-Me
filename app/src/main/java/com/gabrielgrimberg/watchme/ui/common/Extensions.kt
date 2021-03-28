2fa32a44d489ca2a9239ed8a372622c0cd10930e
Gabriel Grimberg <gabrielgrimberg@hotmail.com>
2021-03-28 21:38:58 +0100

    UI Common Utils

---
+++ app/src/main/java/com/gabrielgrimberg/watchme/ui/common/Extensions.kt
@@ -0,0 +1,37 @@
+/**
+ * Extensions.kt provides the ability to extend a class with new functionality without
+ * having to inherit from the class or use any type of design pattern such as Decorator.
+ */
+package com.gabrielgrimberg.watchme.ui.common
+
+import android.view.View
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
+
+inline fun View.snack(message: String, length: Int = Snackbar.LENGTH_INDEFINITE, f: Snackbar.() -> Unit) {
+    val snack = Snackbar.make(this, message, length)
+    snack.f()
+    snack.show()
+}
+
+fun Snackbar.action(action: String, color: Int? = null, listener: (View) -> Unit) {
+    setAction(action, listener)
+    color?.let { setActionTextColor(color) }
+}
+
+/**
+ * BindingAdapter objects are useful for changing the way traditional bindings behave
+ * because they override the traditional adapters that are provided by the Android Framework.
+ *
+ */
+@BindingAdapter("imageUrl")
+fun ImageView.setImageUrl(url: String?) {
+    Picasso.get().load(url).into(this)
+}
+
+@BindingAdapter("imageUrl")
+fun ImageView.setImageUrl(int: Int) {
+    this.setImageDrawable(resources.getDrawable(int,null))
+}
---
+++ app/src/main/java/com/gabrielgrimberg/watchme/ui/common/ViewUtils.kt
@@ -0,0 +1,25 @@
+package com.gabrielgrimberg.watchme.ui.common
+
+import android.content.Context
+import android.content.Intent
+import com.gabrielgrimberg.watchme.ui.home.activity.MainActivity
+import com.gabrielgrimberg.watchme.ui.auth.activity.LoginActivity
+import com.gabrielgrimberg.watchme.ui.movie.activity.AddMovieActivity
+
+fun Context.startHomeActivity() =
+        Intent(this, MainActivity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(it)
+        }
+
+fun Context.startLoginActivity() =
+        Intent(this, LoginActivity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(it)
+        }
+
+fun Context.startAddMovieActivity() =
+        Intent(this, AddMovieActivity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(it)
+        }
